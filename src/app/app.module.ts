c7aab681e6c8eff50a31948f41cbde839ad28797
Vladyslav Bekerskyi <vbekerskij@gmail.com>
2022-02-17 00:02:40 +0200

    feat: generate component friends

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FriendsComponent } from './friends/friends.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FriendsComponent
   ],
   imports: [
     BrowserModule,
