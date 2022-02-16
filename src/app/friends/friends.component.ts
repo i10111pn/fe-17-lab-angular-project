96891b2c640792fe66776e7c96ac6cb927705bbd
Vladyslav Bekerskyi <vbekerskij@gmail.com>
2022-02-17 00:16:29 +0200

    fix: lifecycle methods should not be empty

--- src/app/friends/friends.component.ts
+++ src/app/friends/friends.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-friends',
   templateUrl: './friends.component.html',
   styleUrls: ['./friends.component.scss']
 })
-export class FriendsComponent implements OnInit {
+export class FriendsComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
