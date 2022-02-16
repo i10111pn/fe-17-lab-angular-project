b90dc0abdc9111a6f6aede87054590285a06086f
Yevhen Motrych <i10111pn@gmail.com>
2022-02-16 20:20:49 +0200

    general: initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'fe-17-lab-angular-project';
+}
