66b2278e146d55978fab1b2d617880be5a596a6e
Yevhen Motrych <i10111pn@gmail.com>
2022-02-23 15:40:01 +0200

    general: add material ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 import { FriendsComponent } from './friends/friends.component';
 import { GamesComponent } from './games/games.component';
-
 import { FriendComponent } from './friend/friend.component';
-import { FormsModule} from '@angular/forms';
-
+import { LibraryComponent } from './library/library.component';
 
 import { GamesSearchPipe } from './pipes/games-search.pipe';
 import { IndieFilterPipe } from './pipes/indie-filter.pipe';
 import { ActionFilterPipe } from './pipes/action-filter.pipe';
 import { AdventureFilterPipe } from './pipes/adventure-filter.pipe';
 import { PriceFilterPipe } from './pipes/price-filter.pipe';
-import { LibraryComponent } from './library/library.component'
+
 import { MyGamesService } from './services/my-games.service';
-
-
 
 @NgModule({
   declarations: [
@@ -25,17 +24,18 @@ import { MyGamesService } from './services/my-games.service';
     FriendsComponent,
     GamesComponent,
     FriendComponent,
+    LibraryComponent,
     GamesSearchPipe,
     IndieFilterPipe,
     ActionFilterPipe,
     AdventureFilterPipe,
-    PriceFilterPipe,
-    LibraryComponent,
+    PriceFilterPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NoopAnimationsModule
   ],
   providers: [MyGamesService],
   bootstrap: [AppComponent]
